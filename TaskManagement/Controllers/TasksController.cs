88f17a7f9b5315f5fa82676d7464b19678c72281
Juozas Rastenis <eaajuras5@students.eaaa.dk>
2018-11-26 21:13:05 +0100

    Tasks can be viewed now

--- TaskManagement/Controllers/TasksController.cs
+++ TaskManagement/Controllers/TasksController.cs
@@ -14,6 +14,7 @@ namespace TaskManagement.Controllers
     public class TasksController : Controller
     {
         private readonly TaskManagementSystemContext _context;
+        TaskDetailsViewModel task_details_model = new TaskDetailsViewModel();
 
         public TasksController(TaskManagementSystemContext context)
         {
@@ -29,25 +30,31 @@ namespace TaskManagement.Controllers
         public IActionResult Create(int? id)
         {
             ViewBag.ProjectID = id;
-            ViewData["Creator"] = new SelectList(_context.Account, "AccountId", "AccountEmail");
+            ViewData["TaskCreatorAccountId"] = new SelectList(_context.Account, "AccountId", "AccountEmail");
             ViewData["TaskList"] = new SelectList(_context.TaskState, "TaskStateId", "TaskStateName");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TaskId,TaskName,TaskCreatedDateTime,TaskDescription,TaskCreatorAccountId,TaskTaskStateId,TaskProjectId,TaskTaskStateLastChangeDateTime")] Models.Task task, int ProjectID)
+        public async Task<IActionResult> Create([Bind("TaskId,TaskName,TaskCreatedDateTime,TaskDescription,TaskCreatorAccountId,TaskTaskStateId,TaskProjectId,TaskTaskStateLastChangeDateTime")] Models.Task task, [Bind("AccountId,TaskId")] JAccountTask junction, int ProjectID)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(task);
                 await _context.SaveChangesAsync();
+                junction.TaskId = task.TaskId;
+                _context.Add(junction);
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction("Details", "Projects", new { id = ProjectID });
             }
+            ViewData["TaskCreatorAccountId"] = new SelectList(_context.Account, "AccountId", "AccountEmail");
             ViewData["ProjectCreatorAccountId"] = new SelectList(_context.Account, "AccountId", "AccountEmail", task.TaskCreatorAccountId);
             return View(task);
         }
 
+        // Only render the View for Delete
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -65,15 +72,47 @@ namespace TaskManagement.Controllers
             return View(task);
         }
 
+        // Actually Delete Task
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id, int ProjectID)
         {
             var task = await _context.Task.FindAsync(id);
+            var j_task_acc = _context.JAccountTask.Where(t => t.TaskId == id);
             HttpContext.Session.SetString("DeletedMessage", task.TaskName + " has been successfully deleted");
+
+            _context.JAccountTask.RemoveRange(j_task_acc);
             _context.Task.Remove(task);
             await _context.SaveChangesAsync();
             return RedirectToAction("Details", "Projects", new { id = ProjectID });
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Task.FirstOrDefaultAsync(m => m.TaskId == id);
+
+            List<Account> assignees = await
+                (from acc in _context.Account
+                 join jt in _context.JAccountTask
+                 on acc.AccountId equals jt.AccountId
+                 where jt.TaskId == id
+                 select acc).ToListAsync();
+
+            List<Comment> comments = await _context.Comment.Where(c => c.CommentProjectId == id).ToListAsync();
+
+            task_details_model = new TaskDetailsViewModel
+            {
+                Task = task,
+                Assignees = assignees,
+                Comments = comments
+            };
+
+            return View(task_details_model);
         }
     }
 }
